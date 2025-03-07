184c110c3d2b6e915c44ed189079246b0d1717e8
Tamara Sibileva <sibilevato@gmail.com>
2025-03-08 00:33:11 +1200

    Test passed

--- src/test/java/ApiTestPostman.java
+++ src/test/java/ApiTestPostman.java
@@ -2,17 +2,23 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
 
 public class ApiTestPostman {
     @Test
     void shouldSendPostRequest() {
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data")
+                .contentType("application/json; charset=UTF-8")
+                .body("Hello, world!")
                 .when()
                 .post("/post")
                 .then()
-                .statusCode(300)
+                .statusCode(200)
+                .body("data", equalTo("Hello, world!"))
+                .contentType(ContentType.JSON)
                 ;
     }
 }
